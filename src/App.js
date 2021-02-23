bc857d937f37c7702fcf3707fa7c317a1004b44c
박도희 <dohee.pa@gmail.com>
2021-02-23 22:22:28 +0900

    13.4.1 URL 파라미터

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Profile from './Profile';
 
 const App = () => {
   return (
     <div>
+      <ul>
+        <li><Link to='/'>홈</Link></li>
+        <li><Link to='/about'>소개</Link></li>
+        <li><Link to='/profile/dpark'>dpark profile</Link></li>
+        <li><Link to='/profile/gildong'>gildong profile</Link></li>
+      </ul>
       <Route exact path='/' component={Home} />
       <Route path={['/about', '/info']} component={About} />
+      <Route path={['/profile/:username']} component={Profile} />
     </div>
   );
 };
