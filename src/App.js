db3e952b1ba2aca37217c25fda93fb5b3e3bc362
박도희 <dohee.pa@gmail.com>
2021-03-12 23:02:39 +0900

    13.6.1. history

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Profile from './Profile';
 import Profiles from './Profiles';
+import HistorySample from './HistorySample';
 
 const App = () => {
   return (
@@ -10,11 +12,16 @@ const App = () => {
       <ul>
         <li><Link to='/'>홈</Link></li>
         <li><Link to='/about'>소개</Link></li>
-        <li><Link to='/profiles'>profile</Link></li>
+        <li><Link to='/profile/dpark'>dpark profile</Link></li>
+        <li><Link to='/profile/gildong'>gildong profile</Link></li>
+        <li><Link to='/profiles'>profiles</Link></li>
+        <li><Link to='/history'>History example</Link></li>
       </ul>
       <Route exact path='/' component={Home} />
       <Route path={['/about', '/info']} component={About} />
+      <Route path='/profile/:username' component={Profile} />
       <Route path='/profiles' component={Profiles} />
+      <Route path='/history' component={HistorySample} />
     </div>
   );
 };
--- src/Profile.js
+++ src/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import WithRouterSample from './WithRouterSample';
 
 const data = {
   dpark: {
@@ -25,6 +26,7 @@ const profile = ({ match }) => {
     <div>
       <h3>{username} ({profile.name})</h3>
       <p>{profile.description}</p>
+      <WithRouterSample />
     </div>
   );
 };
