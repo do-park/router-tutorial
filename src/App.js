c37803ab4f84b918175fb58cd7b8826a6d700d8c
박도희 <dohee.pa@gmail.com>
2021-03-12 22:47:55 +0900

    13.5 서브 라우트

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 const App = () => {
   return (
@@ -10,12 +10,11 @@ const App = () => {
       <ul>
         <li><Link to='/'>홈</Link></li>
         <li><Link to='/about'>소개</Link></li>
-        <li><Link to='/profile/dpark'>dpark profile</Link></li>
-        <li><Link to='/profile/gildong'>gildong profile</Link></li>
+        <li><Link to='/profiles'>profile</Link></li>
       </ul>
       <Route exact path='/' component={Home} />
       <Route path={['/about', '/info']} component={About} />
-      <Route path={['/profile/:username']} component={Profile} />
+      <Route path='/profiles' component={Profiles} />
     </div>
   );
 };
